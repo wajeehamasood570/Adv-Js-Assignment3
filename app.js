1fbdc8985192afe29d10a5657eec99ccd5c5baa7
wajeehamasood570 <wajeehamasood570@gmail.com>
2021-09-25 18:11:51 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,67 @@
+
+// adv js
+// q1
+//write a program which shows the couter after each second.
+
+// let time = 1000;
+// let i=0
+// function doStuff(){
+//         console.log(i);
+//         i++;
+//         }
+    
+//     setInterval(doStuff,time)
+    
+
+// q2
+// write a function which takes your name and displays the greeting with your name
+// let name = prompt("Enter your Name","Name");
+
+// function print(){
+//     console.log("Welcome " + name);
+// }
+
+// print();
+
+
+// q3
+// write a function(arrow function) which takes two values and return its sum as a result
+
+// myFunc = (a,b) =>{
+//         let c = a + b;
+//         console.log(c);
+//         return c;
+//     }
+//      myFunc(6,5);
+
+
+// q4
+//write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+
+// myFunc = (a) =>{
+//     let c = a * 0.5;
+//     return c;
+
+// }
+
+// let d = myFunc(8);
+// console.log(d);
+
+
+//q5
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+
+// let num = [1,2,3,4,5]
+// num.map((a)=>console.log(a))
+
+//q6
+// let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
+// let arr = [
+//         {id: 1, name: "abc"},
+//         {id: 2, name: "efg"},
+//         {id: 3, name: "hij"},
+//         {id: 4, name: "xyz"}
+//     ]
+
+// arr.map((a)=>console.log(a));
